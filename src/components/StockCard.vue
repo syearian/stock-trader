d1e6dbc28cf019ac55d85623a392b4853515f8f0
Steven Yearian <syearian@gmail.com>
2019-10-14 11:48:33 -0500

    Add basic Stock Card

--- src/components/StockCard.vue
+++ src/components/StockCard.vue
@@ -1,13 +1,38 @@
 <template>
-  
+  <div class="card">
+    <div class="card-header stockCard--header">
+      <h3 class="is-size-4">{{ stockName }} <span class="is-size-7">(Price: {{ stockPrice }})</span></h3>
+    </div>
+    <div class="card-content stockCard--content">
+      <input class="input" type="text">
+      <button class="button">Buy</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      stockName: 'BMW',
+      stockPrice: 100
+    }
+  },
 }
 </script>
 
-<style>
+<style lang="scss">
+.stockCard--header {
+  padding: .25rem 1rem;
+}
 
+.stockCard--content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  input {
+    width: inherit;
+  }
+}
 </style>
--- src/views/Stocks.vue
+++ src/views/Stocks.vue
@@ -1,10 +1,15 @@
 <template>
-  
+  <StockCard></StockCard>
 </template>
 
 <script>
-export default {
+// @ is an alias to /src
+import StockCard from '@/components/StockCard.vue';
 
+export default {
+  components: {
+    StockCard
+  }
 }
 </script>
 
