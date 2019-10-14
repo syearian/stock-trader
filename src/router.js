56199302aba684b5b2d6be26af9a42b2b4d737b6
Steven Yearian <syearian@gmail.com>
2019-10-14 11:00:39 -0500

    Add initial shell files

--- src/router.js
+++ src/router.js
@@ -14,12 +14,19 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/portfolio',
+      name: 'portfolio',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import( /* webpackChunkName: "portfolio" */ './views/Portfolio.vue')
+    }, {
+      path: '/stocks',
+      name: 'stocks',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import( /* webpackChunkName: "stocks" */ './views/Stocks.vue')
     }
   ]
 })
