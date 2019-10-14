d810708f5e4511099dad3db209a2d84b0629bb24
Steven Yearian <syearian@gmail.com>
2019-10-14 11:07:50 -0500

    Add content to Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div class="box content">
+    <h1>Trade or View your portfolio</h1>
+    <p>You may save and load your data.</p>
+    <p>Click on "End Day" to begin a new day!</p>
+    <hr>
+    <p>Your Funds: ${{ funds }}</p>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'home'
+  name: 'home',
+  data() {
+    return {
+      funds: 10000
+    }
+  }
 }
 </script>
